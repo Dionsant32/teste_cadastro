141e6ec8a397a63c14328206aebc4ed4f477ce78
Dionsant32 <81274513+Dionsant32@users.noreply.github.com>
2021-05-17 19:38:13 -0300

    Update cadastro.js

--- cadastro.js
+++ cadastro.js
@@ -1,6 +1,12 @@
 var atual;
 var localStorage = Window.localStorage;
 
+function menuCadastro(){
+    atual.classList.add("d-none"); //Para esconder o container em apresentação é feita a adição da classe d-none
+    atual = document.getElementById("registro"); //Busca o container que apresenta a tela de cadastro
+    atual.classList.remove("d-none"); //Remove a classe d-none do container de cadastro para que ele fique visível
+}            
+
 function gravar(){ 
     var novo = new Object(); 
     novo.cpf=document.getElementById("cpf").value; 
